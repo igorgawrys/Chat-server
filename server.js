3cbfe30f89c2ee64cec4eb8c93930d306dc6d2ed
Artur Czuba <Artur.Czuba@lionbridge.com>
2017-05-31 16:57:29 +0200

    Change name to chat-server

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 
-app.get('/', function(req, res){
 
-    res.sendFile(__dirname + '/www/index.html');
-
-});
+app.get('/', function(req, res){});
 
 http.listen(3000, function(){
     console.log("Server started at port 3000");
