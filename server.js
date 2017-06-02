a2d4be175ca334f073e40e3587c5cb87d0292a97
Artur Czuba <Artur.Czuba@lionbridge.com>
2017-06-02 11:15:28 +0200

    Added post method to conversations

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const bodyParser = require('body-parser');
 
 // Get default server port
 const port = process.env.port || '3000';
@@ -9,7 +10,8 @@ app.set('port', port);
 
 let allowedOrigins = [
     'http://localhost:3001',
-    'http://localhost:8012'
+    'http://localhost:8012',
+    'http://localhost:4200'
 ]
 
 // Allow cross origin for multiple domains
@@ -20,6 +22,9 @@ app.use((req, res, next) => {
     if(allowedOrigins.indexOf(origin) > -1){
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     next();
 });
@@ -35,32 +40,48 @@ let conversations = {
     1: [
         {
             from: 500,
-            type: 'text',
             content: "Hej!",
             readBy: [ 501 ]
         }, {
             from: 501,
-            type: 'text',
             content: "Witaj!"
         }, {
             from: 501,
-            type: 'image',
             content: 'https://avatars0.githubusercontent.com/u/4172079?v=3&s=88'
         }
     ]
 }
 
-app.get('/messages/:conversation_id', (req, res) => {
-    let conversation_id = req.params.conversation_id;
-    let conversation = [];
+function findConversation(conversation_id){
     for(let i in conversations){
         if(i === conversation_id){
-            conversation = conversations[i];
-            break;
+            return conversations[i];
         }
     }
 
-    res.send(conversation);
+    return [];
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/messages/:conversation_id', (req, res) => {
+    res.send(findConversation(req.params.conversation_id));
+});
+
+app.post('/messages/:conversation_id', (req, res) => {
+
+    let conversation = findConversation(req.params.conversation_id);
+    let newMessage = {
+        from: req.body.from,
+        type: req.body.type,
+        content: req.body.content
+    };
+
+    conversation.push(newMessage);
+    res.send(newMessage);
 });
 
 // SetUp server
